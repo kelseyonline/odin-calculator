920ba4072f019c94be48ebcd1a087bfa438ca6e7
Kelsey Dabrowski <kdabrowski103@gmail.com>
2024-03-12 11:40:53 -0400

    Add placeholder value in HTML

---
+++ script.js
@@ -0,0 +1,30 @@
+// Add a placeholder text in the HTML with a "0"
+    // Create a variable called displayValue to store what should go in the screen
+
+// Create a variable for the first variable, the operator, and the second variable
+
+// Create functions for add(), subtract(), multiply(), and divide()
+
+// Add functionality to each button that adds a number into the screen
+
+// Add functionality to clear button
+    // MAKE SURE THAT WHEN YOU PRESS CLEAR, LITERALLY EVERYTHING IS CLEARED!! INCLUDING ALL VARIABLES
+
+// Add functionality to backspace button
+
+// Add functionality to operators (no evaluations, just adding to screen)
+
+// Add functionality to decimal 
+    // Ensure that decimal becomes invalid when there is already a decimal on the screen 
+
+// Add functionality to equal sign 
+    // When equal sign is pressed, evaluates expression
+    // Create a new operate() function
+    // This will call on ANOTHER function, one of the mathematical functions you created
+    // This will combine the two operands and the operator variables you created before
+    // Expression result shows on screen
+    // An expression with more than 2 numbers should evaluate the expressions two variables at a time (so forget PEMDAS)
+
+// Round answers with long decimals so that they don't overflow the screen
+
+// Display error message if user tries to divide by 0
